000487c666f8c5cf07d3c700b8db9ab6a4d20e7e
Godeos64 <sidhuarman700@gmail.com>
2026-02-16 21:26:27 +0530

    Refactor window management functions and timing

--- Main.py
+++ Main.py
@@ -2,9 +2,8 @@ import time
 from os import _exit
 from pynput import keyboard
 from pynput.keyboard import Key
-import sys
 from PyQt6.QtWidgets import QApplication, QMessageBox
-from GUI import MessageBox # choose one of the 2 themes in the GUI files.
+from GUI_Dark_mode import MessageBox
 # Config
 
 Controller = keyboard.Controller()
@@ -23,22 +22,30 @@ def on_press(key):  # emergency exit
 listener = keyboard.Listener(on_press=on_press)
 listener.start()
 
-def windows_d(): # minimizes/restores all windows by pressing Windows + D
-     Controller.press(Key.cmd)
-     Controller.press("d")
-     Controller.release(Key.cmd)
-     Controller.release("d")
+def minimize_windows(): 
+    # Minimizes all windows using Win + M
+    Controller.press(Key.cmd)
+    Controller.press("m")
+    Controller.release("m")
+    Controller.release(Key.cmd)
+
+def restore_windows():
+     # Restores minimized windows using Shift + Win + M
+    Controller.press(Key.cmd)
+    Controller.press(Key.shift)
+    Controller.press("m")
+    Controller.release("m")
+    Controller.release(Key.shift)
+    Controller.release(Key.cmd)
 
 # Main
 
 while Active:
-     time.sleep(1500) # waits 25 minutes
-     windows_d() # minimizes all windows
+     time.sleep(5) # waits 25 minutes
+     minimize_windows() # minimizes all windows
      MessageBox("Take a break!", "Break Time")
      
 
-     time.sleep(300) # waits 5 minutes 
-
-     windows_d() # restores windows
+     time.sleep(3) # waits 5 minutes 
      MessageBox("Let's get back to work!", "Break Over")
-     
+     restore_windows() # restores windows  
